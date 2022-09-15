72418c36c61420d2ca4cb9c3b26d033f9a2e0ad4
psakowski <p.sakowski1213@gmail.com>
2022-09-15 21:25:47 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,75 @@
-import {settings, select, classNames, templates} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    thisApp.activatePage(thisApp.pages[0].id);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+      }); 
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+
+    // for(let page of thisApp.pages){
+    //   if(page.id == pageId){
+    //     page.classList.add(classNames.pages.active);
+    //   } else {
+    //     page.classList.remove(classNames.pages.active);
+    //   }
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;
     //console.log('thisApp.data:', thisApp.data);
     for(let productData in thisApp.data.products){
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
     }
+  },
+
+  initCart: function(){
+    const thisApp = this;
+
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+
+    thisApp.productList = document.querySelector(select.containerOf.menu);
+
+    thisApp.productList.addEventListener('add-to-cart', function(event){
+      app.cart.add(event.detail.product);
+    });
   },
 
   initData: function(){
@@ -32,28 +93,18 @@ const app = {
 
   init: function(){
     const thisApp = this;
+    /*
     console.log('*** App starting ***');
     console.log('thisApp:', thisApp);
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
+    */
 
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
-
-  initCart: function(){
-    const thisApp = this;
-
-    const cartElem = document.querySelector(select.containerOf.cart);
-    thisApp.cart = new Cart(cartElem);
-
-    thisApp.productList = document.querySelector(select.containerOf.menu);
-
-    thisApp.productList.addEventListener('add-to-cart', function(event){
-      app.cart.add(event.detail.product);
-    });
-  }
 };
 
 app.init();
